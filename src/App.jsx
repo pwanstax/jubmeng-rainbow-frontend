e36f3ccf588beaa308a7d51d1a7d6b4ce9ad12a5
stam <stamp.chayakorn@gmail.com>
2023-02-10 05:30:12 +0700

    clinic page filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import AboutUsPage from "./pages/AboutUsPage";
 import NoPage from "./pages/NoPage";
+import ClinicPage from "./pages/ClinicPage";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" exact element={<ClinicPage />} />
           <Route path="/" exact element={<HomePage />} />
           <Route path="/about" element={<AboutUsPage />} />
           <Route path="*" element={<NoPage />} />
--- src/pages/ClinicPage.jsx
+++ src/pages/ClinicPage.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, {useState} from "react";
 
 const ClinicPage = () => {
+  const demo = {
+    pet: {cat: false, dog: false, bird: true, lion: true},
+    type: {clinic: true, place: false, service: false},
+  };
+  const [filterList, setFilterList] = useState(demo);
+
   return (
-    <div>
-        Clinic
+    <div className="clinic-page-container">
+      <div className="header">
+        <h1>Clinics for Pets</h1>
+      </div>
+      <div className="padding-box">
+        <div className="content-box">
+          <div className="filter-box">
+            {Object.keys(filterList).map((key, index) => {
+              return (
+                <>
+                  <div className="category-box" key={`${key}${index}`}>
+                    <h3>{key}</h3>
+                    
+                    {Object.keys(filterList[key]).map((k, i) => {
+                      return (
+                        <label>
+                          <input
+                            type="checkbox"
+                            id={`${key}${k}${i}`}
+                            name={k}
+                            value={k}
+                            checked={filterList[key][k]}
+                            //   onChange={handleOnChange}
+                          />
+                          {k}
+                        </label>
+                      );
+                    })}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+          <div className="result-box">result</div>
+        </div>
+      </div>
     </div>
-  )
-}
-export default ClinicPage
+  );
+};
+
+export default ClinicPage;
