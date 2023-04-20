2c68148c7419279400a40162c17a710b62691269
Kantaponn <78014536+Kantaponn@users.noreply.github.com>
2023-04-21 01:47:01 +0700

    new recommend places on home page

--- src/components/ContentSlide.jsx
+++ src/components/ContentSlide.jsx
@@ -1,45 +1,68 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import axios from "axios";
 import HomeProductCard from "./HomeProductCard";
 
-const ContentSlide = ({topic, icon, searchQuery}) => {
+const ContentSlide = ({topic, icon, searchQuery, latitude, longitude}) => {
   const [contents, setContents] = useState([]);
+  const latestUpdateAt = useRef(null);
 
   useEffect(() => {
     const fetchContent = async () => {
+      const updateAt = new Date();
+      let params = {sort: searchQuery};
+      if (latitude && longitude) {
+        params.latitude = latitude;
+        params.longitude = longitude;
+      }
+      latestUpdateAt.current = updateAt;
       try {
         const res = await axios.get(
-          `http://localhost:8080/products?name=${searchQuery}`
+          `http://localhost:8080/products/recommend/`,
+          {
+            params: params,
+          }
         ); // change path to backend service
-        setContents(res.data);
+        if (latestUpdateAt.current === updateAt) {
+          setContents(res.data);
+        }
       } catch (error) {
         console.error(error);
       }
     };
     fetchContent();
-  }, [searchQuery]);
+  }, [searchQuery, latitude, longitude]);
 
   return (
-    <div className="content-slide-container">
-      <h1>
-        <i className={`fa-solid fa-${icon}`}></i>
-        {topic}
-      </h1>
-      <div className="cards-wrap">
-        {contents.map((content, index) => {
-          return (
-            <HomeProductCard
-              id={content.id}
-              name={content.name}
-              locationDescription={content.locationDescription}
-              tags={content.tags}
-              image={content.image}
-              key={index}
-            />
-          );
-        })}
-      </div>
-    </div>
+    <>
+      {searchQuery == "closest_location" && (!latitude || !longitude) ? (
+        <></>
+      ) : (
+        <div className="content-slide-container">
+          <h1>
+            <i className={`fa-solid fa-${icon}`}></i>
+            {topic}
+          </h1>
+          <div className="cards-wrap">
+            {contents.map((content, index) => {
+              return (
+                // <div>{content.distance || 0}</div>
+                <HomeProductCard
+                  id={content.id}
+                  name={content.name}
+                  locationDescription={content.locationDescription}
+                  tags={content.tags}
+                  image={content.image}
+                  distance={content.distance || null}
+                  rating={content.rating}
+                  reviewCounts={content.reviewCounts}
+                  key={index}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
